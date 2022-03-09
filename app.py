58378468ad5caeb60e809f6ce421d0d3618f3aef
Gerasimov Roman <romagrizly@gmail.com>
2022-03-09 17:53:24 +0500

    add login

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, url_for, render_template, redirect, request, make_response, session
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, login_required, logout_user
 
 from data import db_session
 from data.db_session import global_init
@@ -116,12 +116,19 @@ def login():
         user = db_sess.query(User).filter(User.email == form.email.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
+            print(session)
             return redirect("/")
         return render_template('login.html',
                                message="Неправильный логин или пароль",
                                form=form)
     return render_template('login.html', title='Авторизация', form=form)
 
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 @login_manager.user_loader
 def load_user(user_id):
